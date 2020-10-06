5ff03b094b1e9d5aa052f51daf670c6a42f0cce7
jerzky <jensg90@gmail.com>
2020-10-06 15:30:05 +0200

    added mac10 and fixed small ui bug

---
+++ Assets/Scripts/GameScene/Items/Mac10.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Assets.Items;
+
+public class MAC10 : Gun
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        ammoUID = ItemList.ITEM_PISTOL_AMMO.UID;
+        noAmmoAudio = Resources.Load<AudioClip>("Sounds/noammo");
+        reloadAudio = Resources.Load<AudioClip>("Sounds/ak_reload");
+        shootAudio = Resources.Load<AudioClip>("Sounds/mac10_fire");
+        rateOfFire = (60f/1000f);
+        cantShootTimer = rateOfFire;
+        reloadSpeed = 3.2f; // lowest reload time which allows sound to finish = 4.5f
+
+        magSize = 30;
+        currentMagSize = 30;
+        damage = ItemList.ITEM_AK47.HumanDamage;
+        bulletPrefab = Resources.Load<GameObject>("Prefabs/bullet");
+        if (bulletPrefab == null)
+            Debug.Log("BULLET NULL????");
+    }
+
+    void Update()
+    {
+        if (cantShootTimer >= 0)
+            cantShootTimer -= Time.deltaTime;
+    }
+}
